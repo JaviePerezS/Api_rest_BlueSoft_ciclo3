8f4d9e2b89ee3e305bf393e8173b2f72e4e37304
JaviePerezS <fjavier2411@gmail.com>
2022-08-18 00:04:08 -0500

    update

---
+++ src/main/java/com/bluesoft/ApiRestBlueSoftCiclo3Application.java
@@ -0,0 +1,13 @@
+package com.bluesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBlueSoftCiclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBlueSoftCiclo3Application.class, args);
+	}
+
+}
