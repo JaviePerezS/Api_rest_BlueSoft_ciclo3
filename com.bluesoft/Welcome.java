318f2fe127c7c0ec01adcc63882df2072203311e
GeraldinE2022 <97415604+GeraldinE2022@users.noreply.github.com>
2022-08-21 16:24:58 -0500

    commit geraldine

---
+++ com.bluesoft/Welcome.java
@@ -0,0 +1,13 @@
+package com.bluesoft;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Francisco Javier Perez Solano");
+		System.out.println("Duvan Vargas Ramirez");
+		System.out.println("Geraldine Alvarez Chabur");
+
+	}
+
+}
