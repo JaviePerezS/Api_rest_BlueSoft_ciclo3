3255dd07a7c0b791c2089de863682648e990989a
JaviePerezS <fjavier2411@gmail.com>
2022-09-01 03:21:38 -0500

    Estructura Base

--- src/main/java/com/bluesoft/ApiApplication.java
+++ src/main/java/com/bluesoft/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRestBlueSoftCiclo3Application {
+public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiRestBlueSoftCiclo3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 
 }
